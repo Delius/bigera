a32793b24ffa30b4787cf3067deb132e4c57869d
delius <pawel@fibrotrust.org>
2014-08-16 12:48:43 +0100

    devise cancan setup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
-class DashboardController < ApplicationController
+class Admin::DashboardController < ApplicationController
+ before_filter :authenticate_user!
+
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-
+  
+  devise_for :users
+  namespace :admin do
+    get '', to: 'dashboard#index', as: '/'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
